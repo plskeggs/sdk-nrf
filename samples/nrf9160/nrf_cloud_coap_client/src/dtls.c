275c97612ab1364a1470f038ba56ce3c7b2344a2
Pete Skeggs <peter.skeggs@nordicsemi.no>
2022-06-02 09:29:03 -0700

    west.yml: Point to sdk-mbedtls and zephyr
    Use changes needed to support DTLS CoAP.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>
    
    samples: nrf9160: coap_client: Add DTLS 1.2 PSK and CID support
    
    Use either the full offloaded modem stack or a mix of mbed TLS
    for DTLS and modem for UDP.
    
    Add client_put_send().  Enable PSM mode.
    
    Split out the dtls code to dtls.c.  Clean up main.c. Make app-level
    Kconfig entries for COAP_DTLS, PSK identity, secret, and sectag.
    Switch server port if COAP_DTLS is enabled.
    
    Provide config overlays for mbed TLS vs. modem stack.  Also
    provide config overlay for debugging.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>
    
    samples: nrf9160: nrf_cloud_coap_client: Add new sample
    
    Initial cut at nRF Cloud CoAP client.
    
    Get modem info IMEI and mfw. Provision psks to modem or mbedtls
    depending on build type. Init the modem, which waits for LTE
    connection. Look up server IP. Init the client, including
    looking up device IP with modem info.
    
    Send fake cell pos parameters with GET, expect response back (response
    not decoded yet). Send fake temperature with PUT. Add CBOR decode of
    cell location. Implement CON handling. Implement message ID and token
    matching based on type of response.
    
    Send CoAP RESET on unmatched response rather than quit.
    
    Still to do: doing exponential retries, gracefully
    handling duplicate packets due to server retransmissions.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>

---
+++ samples/nrf9160/nrf_cloud_coap_client/src/dtls.c
@@ -0,0 +1,302 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/net/coap.h>
+#include <zephyr/net/socket.h>
+#include <modem/lte_lc.h>
+#include <modem/modem_key_mgmt.h>
+#if defined(CONFIG_MODEM_INFO)
+#include <modem/modem_info.h>
+#endif
+#if defined(CONFIG_MBEDTLS)
+#include <mbedtls/ssl.h>
+#include <mbedtls/debug.h>
+#endif
+#if defined(CONFIG_TLS_CREDENTIALS)
+#include <zephyr/net/tls_credentials.h>
+#endif
+#include <nrf_socket.h>
+#include <nrf_modem_at.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(nrf_cloud_coap_client, CONFIG_NRF_CLOUD_COAP_CLIENT_LOG_LEVEL);
+
+#if defined(CONFIG_MBEDTLS)
+/* Uncomment to limit cipher negotation to a list */
+#define RESTRICT_CIPHERS
+/* Uncomment to display the cipherlist available */
+/* #define DUMP_CIPHERLIST */
+#endif
+
+static sec_tag_t sectag = CONFIG_COAP_SECTAG;
+
+#if defined(CONFIG_MODEM_INFO)
+static struct modem_param_info mdm_param;
+
+static int get_modem_info(void)
+{
+	int err;
+
+	err = modem_info_init();
+	if (err) {
+		printk("Could not init modem info: %d\n", err);
+		return err;
+	}
+
+	err = modem_info_string_get(MODEM_INFO_IMEI,
+				    mdm_param.device.imei.value_string,
+				    MODEM_INFO_MAX_RESPONSE_SIZE);
+	if (err <= 0) {
+		printk("Could not get IMEI: %d\n", err);
+		return err;
+	}
+
+	err = modem_info_string_get(MODEM_INFO_FW_VERSION,
+				    mdm_param.device.modem_fw.value_string,
+				    MODEM_INFO_MAX_RESPONSE_SIZE);
+	if (err <= 0) {
+		printk("Could not get mfw ver: %d\n", err);
+		return err;
+	}
+
+	LOG_DBG("IMEI:                    %s", mdm_param.device.imei.value_string);
+	LOG_DBG("Modem FW version:        %s", mdm_param.device.modem_fw.value_string);
+
+	return 0;
+}
+
+static int get_device_ip_address(uint8_t *d4_addr)
+{
+	int err;
+
+	err = modem_info_init();
+	if (err) {
+		return err;
+	}
+
+	err = modem_info_string_get(MODEM_INFO_IP_ADDRESS,
+				    mdm_param.network.ip_address.value_string,
+				    MODEM_INFO_MAX_RESPONSE_SIZE);
+	if (err <= 0) {
+		printk("Could not get IP addr: %d\n", err);
+		return err;
+	}
+
+	err = inet_pton(AF_INET, mdm_param.network.ip_address.value_string, d4_addr);
+	if (err == 1) {
+		return 0;
+	}
+	return errno;
+}
+#endif /* CONFIG_MODEM_INFO */
+
+int provision_psk(void)
+{
+	int ret;
+	static char identity[120];
+	uint16_t identity_len;
+	const char *psk;
+	uint16_t psk_len;
+
+	printk("Provisioning PSK to sectag %u\n", sectag);
+
+	/* get IMEI so we can construct the PSK identity */
+	ret = get_modem_info();
+	if (ret) {
+		return ret;
+	}
+
+	if (strlen(CONFIG_COAP_DTLS_PSK_IDENTITY)) {
+		strncpy(identity, CONFIG_COAP_DTLS_PSK_IDENTITY, sizeof(identity) - 1);
+	} else {
+		const char *identity_fmt = "n%s0123456789abcdef";
+
+		snprintf(identity, sizeof(identity), identity_fmt,
+			 mdm_param.device.imei.value_string);
+	}
+
+	if (strlen(CONFIG_COAP_DTLS_PSK_SECRET)) {
+		psk = CONFIG_COAP_DTLS_PSK_SECRET;
+	} else {
+		psk = "000102030405060708090a0b0c0d0e0f";
+	}
+
+	identity_len = strlen(identity);
+
+	LOG_DBG("psk identity: %s len %u", identity, identity_len);
+
+	psk_len = strlen(psk);
+	LOG_HEXDUMP_DBG(psk, psk_len, "psk");
+
+#if defined(CONFIG_NET_SOCKETS_OFFLOAD_TLS)
+	char psk_hex[65];
+
+	/* Convert PSK to a format accepted by the modem. */
+	psk_len = bin2hex(psk, psk_len, psk_hex, sizeof(psk_hex));
+	if (psk_len == 0) {
+		LOG_ERR("PSK is too large to convert (%d)", -EOVERFLOW);
+		return -EOVERFLOW;
+	}
+	LOG_HEXDUMP_INF(psk_hex, 64, "psk_hex");
+
+	lte_lc_offline();
+
+	ret = modem_key_mgmt_write(sectag, MODEM_KEY_MGMT_CRED_TYPE_PSK, psk_hex, psk_len);
+	if (ret < 0) {
+		LOG_ERR("Setting cred tag %d type %d failed (%d)", sectag,
+			(int)MODEM_KEY_MGMT_CRED_TYPE_PSK, ret);
+		goto exit;
+	}
+
+	ret = modem_key_mgmt_write(sectag, MODEM_KEY_MGMT_CRED_TYPE_IDENTITY, identity,
+				   identity_len);
+	if (ret < 0) {
+		LOG_ERR("Setting cred tag %d type %d failed (%d)", sectag,
+			(int)MODEM_KEY_MGMT_CRED_TYPE_IDENTITY, ret);
+	}
+exit:
+
+#else
+	ret = tls_credential_add(sectag, TLS_CREDENTIAL_PSK, psk, psk_len);
+	if (ret) {
+		LOG_ERR("Failed to set PSK: %d", ret);
+	}
+	ret = tls_credential_add(sectag, TLS_CREDENTIAL_PSK_ID, identity, identity_len);
+	if (ret) {
+		LOG_ERR("Failed to set PSK identity: %d", ret);
+	}
+#endif
+	return ret;
+}
+
+int client_dtls_init(int sock)
+{
+	int err;
+	uint8_t d4_addr[4];
+#if defined(RESTRICT_CIPHERS)
+	static const int ciphers[] = {
+		MBEDTLS_TLS_PSK_WITH_AES_128_CCM_8, 0
+	};
+#endif
+
+	err = get_device_ip_address(d4_addr);
+	if (err) {
+		return err;
+	}
+	printk("Client IP address: %u.%u.%u.%u\n", d4_addr[0], d4_addr[1], d4_addr[2], d4_addr[3]);
+
+	printk("Setting socket options\n");
+
+	err = setsockopt(sock, SOL_TLS, TLS_HOSTNAME, CONFIG_COAP_SERVER_HOSTNAME,
+			 sizeof(CONFIG_COAP_SERVER_HOSTNAME));
+	if (err) {
+		printk("Error setting hostname: %d\n", errno);
+		return err;
+	}
+
+	err = setsockopt(sock, SOL_TLS, TLS_SEC_TAG_LIST, &sectag, sizeof(sec_tag_t));
+	if (err) {
+		printk("Error setting sectag list: %d\n", errno);
+		return err;
+	}
+
+#if defined(RESTRICT_CIPHERS)
+	err = setsockopt(sock, SOL_TLS, TLS_CIPHERSUITE_LIST, ciphers, sizeof(ciphers));
+	if (err) {
+		printk("Error setting cipherlist: %d\n", errno);
+		return err;
+	}
+#endif
+#if defined(DUMP_CIPHERLIST)
+	int len;
+	int ciphers[32];
+
+	len = sizeof(ciphers);
+	err = getsockopt(sock, SOL_TLS, TLS_CIPHERSUITE_LIST, ciphers, &len);
+	if (err) {
+		printk("Error getting cipherlist: %d\n", errno);
+	} else {
+		int count = len / sizeof(int);
+
+		printk("New cipherlist:\n");
+		for (int i = 0; i < count; i++) {
+			printk("%d. 0x%04X = %s\n", i, (unsigned int)ciphers[i],
+			       mbedtls_ssl_get_ciphersuite_name(ciphers[i]));
+		}
+	}
+#endif
+
+#if defined(CONFIG_MBEDTLS_SSL_DTLS_CONNECTION_ID)
+	uint8_t dummy;
+
+	err = setsockopt(sock, SOL_TLS, TLS_DTLS_CONNECTION_ID, &dummy, 0);
+	if (err) {
+		printk("Error setting connection ID: %d\n", errno);
+	}
+#endif
+	return err;
+}
+
+int client_print_connection_id(int sock, bool verbose)
+{
+	int err = 0;
+#if defined(CONFIG_MBEDTLS_SSL_DTLS_CONNECTION_ID)
+	static struct tls_dtls_peer_cid last_cid;
+	struct tls_dtls_peer_cid cid;
+	int cid_len = 0;
+	int i;
+
+	cid_len = sizeof(cid);
+	memset(&cid, 0, cid_len);
+	err = getsockopt(sock, SOL_TLS, TLS_DTLS_PEER_CONNECTION_ID, &cid, &cid_len);
+	if (!err) {
+		if (last_cid.enabled != cid.enabled) {
+			LOG_WRN("CID ENABLE CHANGED");
+		}
+		if (last_cid.peer_cid_len != cid.peer_cid_len) {
+			LOG_WRN("CID LEN CHANGED from %d to %d", last_cid.peer_cid_len,
+			       cid.peer_cid_len);
+			if (cid.peer_cid_len) {
+				printk("DTLS CID IS  enabled:%d, len:%d ", cid.enabled,
+				       cid.peer_cid_len);
+				for (i = 0; i < cid.peer_cid_len; i++) {
+					printk("0x%02x ", cid.peer_cid[i]);
+				}
+				printk("\n");
+			}
+		} else {
+			if (memcmp(last_cid.peer_cid, cid.peer_cid, cid.peer_cid_len) != 0) {
+				LOG_WRN("CID CHANGED!");
+
+				printk("DTLS CID WAS enabled:%d, len:%d ", last_cid.enabled,
+				       last_cid.peer_cid_len);
+				for (i = 0; i < last_cid.peer_cid_len; i++) {
+					printk("0x%02x ", last_cid.peer_cid[i]);
+				}
+				printk("\n");
+			}
+			if (verbose) {
+				printk("DTLS CID IS  enabled:%d, len:%d ", cid.enabled,
+				       cid.peer_cid_len);
+				for (i = 0; i < cid.peer_cid_len; i++) {
+					printk("0x%02x ", cid.peer_cid[i]);
+				}
+				printk("\n");
+			}
+		}
+		memcpy(&last_cid, &cid, sizeof(last_cid));
+	} else {
+		LOG_ERR("Unable to get connection ID: %d", -errno);
+	}
+#else
+	LOG_INF("Skipping CID");
+#endif
+	return err;
+}
