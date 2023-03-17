dbcea6da5b629cece641b46db45b7165df9f712f
Pete Skeggs <peter.skeggs@nordicsemi.no>
2023-03-17 10:58:05 -0700

    samples: nrf9160: coap_client: Add DTLS 1.2 PSK and CID support
    Use either the full offloaded modem stack or a mix of mbed TLS
    for DTLS and modem for UDP.
    
    Add client_put_send().  Enable PSM mode.
    
    Split out the dtls code to dtls.c.  Clean up main.c. Make app-level
    Kconfig entries for COAP_DTLS, PSK identity, secret, and sectag.
    Switch server port if COAP_DTLS is enabled.
    
    Provide config overlays for mbed TLS vs. modem stack.  Also
    provide config overlay for debugging.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>

--- samples/nrf9160/coap_client/src/main.c
+++ samples/nrf9160/coap_client/src/main.c
@@ -12,10 +12,17 @@
 #include <zephyr/net/socket.h>
 #include <modem/lte_lc.h>
 #include <zephyr/random/rand32.h>
+#include <nrf_socket.h>
+#include <nrf_modem_at.h>
 
-#define APP_COAP_SEND_INTERVAL_MS 5000
+#include <logging/log.h>
+LOG_MODULE_REGISTER(app_coap_client, CONFIG_APP_LOG_LEVEL);
+
+#define APP_COAP_SEND_INTERVAL_MS 10000
+#define APP_COAP_INTERVAL_LIMIT 360
 #define APP_COAP_MAX_MSG_LEN 1280
 #define APP_COAP_VERSION 1
+#define APP_COAP_TYPE COAP_TYPE_CON // COAP_TYPE_NON_CON
 
 static int sock;
 static struct pollfd fds;
@@ -23,6 +30,14 @@ static struct sockaddr_storage server;
 static uint16_t next_token;
 
 static uint8_t coap_buf[APP_COAP_MAX_MSG_LEN];
+
+static struct connection_info
+{
+	uint8_t s4_addr[4];
+	uint8_t d4_addr[4];
+} connection_info;
+
+int client_dtls_init(int sock);
 
 /**@brief Resolves the configured hostname. */
 static int server_resolve(void)
@@ -54,9 +69,15 @@ static int server_resolve(void)
 	server4->sin_family = AF_INET;
 	server4->sin_port = htons(CONFIG_COAP_SERVER_PORT);
 
+	connection_info.s4_addr[0] = server4->sin_addr.s4_addr[0];
+	connection_info.s4_addr[1] = server4->sin_addr.s4_addr[1];
+	connection_info.s4_addr[2] = server4->sin_addr.s4_addr[2];
+	connection_info.s4_addr[3] = server4->sin_addr.s4_addr[3];
+
 	inet_ntop(AF_INET, &server4->sin_addr.s_addr, ipv4_addr,
 		  sizeof(ipv4_addr));
-	printk("IPv4 Address found %s\n", ipv4_addr);
+	printk("IPv4 Address for %s:%u found %s\n",
+	       CONFIG_COAP_SERVER_HOSTNAME, CONFIG_COAP_SERVER_PORT, ipv4_addr);
 
 	/* Free the address. */
 	freeaddrinfo(result);
@@ -69,17 +90,34 @@ static int client_init(void)
 {
 	int err;
 
+	LOG_DBG("Creating socket");
+#if !defined(CONFIG_COAP_DTLS)
 	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+#else
+#if defined(CONFIG_NET_SOCKETS_OFFLOAD_TLS)
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_DTLS_1_2);
+#else
+	sock = socket(AF_INET, SOCK_DGRAM | SOCK_NATIVE_TLS, IPPROTO_DTLS_1_2);
+#endif
+#endif
 	if (sock < 0) {
-		printk("Failed to create CoAP socket: %d.\n", errno);
+		printk("Failed to create CoAP socket: %d.\n", -errno);
 		return -errno;
 	}
 
+#if defined(CONFIG_COAP_DTLS)
+	err = client_dtls_init(sock);
+	if (err) {
+		return err;
+	}
+#endif
 	err = connect(sock, (struct sockaddr *)&server,
 		      sizeof(struct sockaddr_in));
 	if (err < 0) {
-		printk("Connect failed : %d\n", errno);
+		printk("Connect failed : %d\n", -errno);
 		return -errno;
+	} else {
+		LOG_DBG("Connect succeeded.");
 	}
 
 	/* Initialize FDS, for poll. */
@@ -88,6 +126,26 @@ static int client_init(void)
 
 	/* Randomize token. */
 	next_token = sys_rand32_get();
+
+	return 0;
+}
+
+static int client_response(struct coap_packet *req)
+{
+	int err;
+	struct coap_packet response;
+
+	err = coap_ack_init(&response, req, coap_buf, sizeof(coap_buf), 0);
+	if (err < 0) {
+		printk("Failed to create CoAP response, %d\n", err);
+		return err;
+	}
+
+	err = send(sock, response.data, response.offset, 0);
+	if (err < 0) {
+		printk("Failed to send CoAP response, %d\n", -errno);
+		return -errno;
+	}
 
 	return 0;
 }
@@ -101,7 +159,8 @@ static int client_handle_get_response(uint8_t *buf, int received)
 	uint16_t payload_len;
 	uint8_t token[8];
 	uint16_t token_len;
-	uint8_t temp_buf[16];
+	uint8_t temp_buf[16] = {0};
+	uint8_t type;
 
 	err = coap_packet_parse(&reply, buf, received, NULL, 0);
 	if (err < 0) {
@@ -111,16 +170,17 @@ static int client_handle_get_response(uint8_t *buf, int received)
 
 	payload = coap_packet_get_payload(&reply, &payload_len);
 	token_len = coap_header_get_token(&reply, token);
+	type = coap_header_get_type(&reply);
 
 	if ((token_len != sizeof(next_token)) ||
 	    (memcmp(&next_token, token, sizeof(next_token)) != 0)) {
-		printk("Invalid token received: 0x%02x%02x\n",
-		       token[1], token[0]);
+		printk("Invalid token received: 0x%02x%02x, expected: 0x%04x\n",
+		       token[1], token[0], next_token);
 		return 0;
 	}
 
 	if (payload_len > 0) {
-		snprintf(temp_buf, MIN(payload_len, sizeof(temp_buf)), "%s", payload);
+		snprintf(temp_buf, MIN(payload_len + 1, sizeof(temp_buf)), "%s", payload);
 	} else {
 		strcpy(temp_buf, "EMPTY");
 	}
@@ -128,10 +188,14 @@ static int client_handle_get_response(uint8_t *buf, int received)
 	printk("CoAP response: code: 0x%x, token 0x%02x%02x, payload: %s\n",
 	       coap_header_get_code(&reply), token[1], token[0], temp_buf);
 
-	return 0;
+	if (type == COAP_TYPE_CON) {
+		printk("Sending ACK\n");
+		err = client_response(&reply);
+	}
+	return err;
 }
 
-/**@biref Send CoAP GET request. */
+/**@brief Send CoAP GET request. */
 static int client_get_send(void)
 {
 	int err;
@@ -140,7 +204,7 @@ static int client_get_send(void)
 	next_token++;
 
 	err = coap_packet_init(&request, coap_buf, sizeof(coap_buf),
-			       APP_COAP_VERSION, COAP_TYPE_NON_CON,
+			       APP_COAP_VERSION, APP_COAP_TYPE,
 			       sizeof(next_token), (uint8_t *)&next_token,
 			       COAP_METHOD_GET, coap_next_id());
 	if (err < 0) {
@@ -149,8 +213,8 @@ static int client_get_send(void)
 	}
 
 	err = coap_packet_append_option(&request, COAP_OPTION_URI_PATH,
-					(uint8_t *)CONFIG_COAP_RESOURCE,
-					strlen(CONFIG_COAP_RESOURCE));
+					(uint8_t *)CONFIG_COAP_GET_RESOURCE,
+					strlen(CONFIG_COAP_GET_RESOURCE));
 	if (err < 0) {
 		printk("Failed to encode CoAP option, %d\n", err);
 		return err;
@@ -167,6 +231,54 @@ static int client_get_send(void)
 	return 0;
 }
 
+/**@brief Send CoAP PUT request. */
+static int client_put_send(void)
+{
+	int err;
+	struct coap_packet request;
+	const char *payload = "deadbeef";
+
+	next_token++;
+
+	err = coap_packet_init(&request, coap_buf, sizeof(coap_buf),
+			       APP_COAP_VERSION, APP_COAP_TYPE,
+			       sizeof(next_token), (uint8_t *)&next_token,
+			       COAP_METHOD_PUT, coap_next_id());
+	if (err < 0) {
+		printk("Failed to create CoAP request, %d\n", err);
+		return err;
+	}
+
+	err = coap_packet_append_option(&request, COAP_OPTION_URI_PATH,
+					(uint8_t *)CONFIG_COAP_PUT_RESOURCE,
+					strlen(CONFIG_COAP_PUT_RESOURCE));
+	if (err < 0) {
+		printk("Failed to encode CoAP option, %d", err);
+		return err;
+	}
+
+	err = coap_packet_append_payload_marker(&request);
+	if (err < 0) {
+		printk("Failed to add CoAP payload marker, %d", err);
+		return err;
+	}
+
+	err = coap_packet_append_payload(&request, payload, strlen(payload));
+	if (err < 0) {
+		printk("Failed to add CoAP payload, %d", err);
+		return err;
+	}
+
+	err = send(sock, request.data, request.offset, 0);
+	if (err < 0) {
+		printk("Failed to send CoAP request, %d", errno);
+		return -errno;
+	}
+
+	printk("CoAP request sent: token 0x%04x\n", next_token);
+
+	return 0;
+}
 /**@brief Configures modem to provide LTE link. Blocks until link is
  * successfully established.
  */
@@ -226,7 +338,9 @@ static int wait(int timeout)
 void main(void)
 {
 	int64_t next_msg_time = APP_COAP_SEND_INTERVAL_MS;
-	int err, received;
+	int err;
+	int received;
+	bool do_send = false;
 
 	printk("The nRF CoAP client sample started\n");
 
@@ -246,11 +360,20 @@ void main(void)
 
 	while (1) {
 		if (k_uptime_get() >= next_msg_time) {
-			if (client_get_send() != 0) {
-				printk("Failed to send GET request, exit...\n");
-				break;
+			if (do_send) {
+				printk("\nCalling client_put_send()\n");
+				if (client_put_send() != 0) {
+					printk("Failed to send PUT request, exit...\n");
+					break;
+				}
+			} else {
+				printk("\nCalling client_get_send()\n");
+				if (client_get_send() != 0) {
+					printk("Failed to send GET request, exit...\n");
+					break;
+				}
 			}
-
+			do_send = !do_send;
 			next_msg_time += APP_COAP_SEND_INTERVAL_MS;
 		}
 
